6cd2001a7831a5315d65c063a53ea7e316f9ae37
lisamrl2 <limaleh@web.de>
2023-11-23 20:49:44 +0100

    made bingo somewhat square-ish and state full

--- files/content.js
+++ files/content.js
@@ -1,16 +1,52 @@
-var cells = 24;
+const cells = 25;
 
 function loadWords() {
+	if (localStorage.getItem("cell"+0) !== null) {
+		loadWordsFromStorage();
+	}
+	else {
+		loadWordsClean();
+	}
+}
+
+function loadWordsClean() {
 	var bullshits = words.split("\n")
 						 .filter(word => word.length > 0)
 						 .map(word => word.trim());
 	
 	var randomizedWords = randomize(bullshits);
-	var counter=0;
-	while (counter<cells) {
-		document.getElementById('cell' + counter++).innerHTML = randomizedWords.pop();
+	
+	for (let counter=0; counter<cells; counter++) {
+		var actu_word = randomizedWords.pop();
+		localStorage.setItem("cell"+counter,actu_word);
+		localStorage.setItem("cell"+counter+"_clicked","0");
+		document.getElementById('cell' + counter).className = "";
+		document.getElementById('cell' + counter).innerHTML = actu_word; 
 	}
 }
+
+function loadWordsFromStorage() {
+	for (let counter=0; counter<cells; counter++) {
+		var element = document.getElementById('cell' + counter);
+		var actu_word = localStorage.getItem("cell"+counter);
+
+		element.innerHTML = actu_word; 
+		var already_clicked = "";
+		var x = localStorage.getItem("cell"+counter+"_clicked");
+		console.log(x)
+		if (x!=="0") {
+			element.className = "clicked";
+		}
+		
+	}
+	var first = document.getElementById('cell' + 0);
+	first.click();
+	first.click();
+}
+
+
+
+
 
 function randomize(words) {
 	var randomizedWords = [];
