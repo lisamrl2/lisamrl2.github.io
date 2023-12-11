dd195ba2294637f2fa7af8618e477eabedf28459
lisamrl2 <limaleh@web.de>
2023-12-11 10:26:35 +0100

    language change

--- files/content.js
+++ files/content.js
@@ -1,69 +1,103 @@
 const cells = 25;
 
 function loadWords() {
-	if (localStorage.getItem("cell"+0) !== null) {
-		loadWordsFromStorage();
-	}
-	else {
-		loadWordsClean();
-	}
+  const full_list = localStorage.getItem("full_list");
+  if (full_list !== null) {
+    loadWordsFromStorage();
+  } else {
+    loadWordsClean();
+  }
 }
 
 function loadWordsClean() {
-	var bullshits = words.split("\n")
-						 .filter(word => word.length > 0)
-						 .map(word => word.trim());
-	
-	var randomizedWords = randomize(bullshits);
-	
-	for (let counter=0; counter<cells; counter++) {
-		var actu_word = randomizedWords.pop();
-		localStorage.setItem("cell"+counter,actu_word);
-		localStorage.setItem("cell"+counter+"_clicked","0");
-		document.getElementById('cell' + counter).className = "";
-		document.getElementById('cell' + counter).innerHTML = actu_word; 
-	}
+  const randomizedWords = randomize();
+  localStorage.setItem("full_list", JSON.stringify(randomizedWords));
+  console.log(randomizedWords);
+  var rWords = [];
+
+  if (localStorage.getItem("language") === "en") {
+    rWords = randomizedWords.en;
+  } else {
+    rWords = randomizedWords.de;
+  }
+
+  for (let counter = 0; counter < cells; counter++) {
+    var actu_word = rWords[counter];
+    //localStorage.setItem("cell" + counter, actu_word);
+    localStorage.setItem("cell" + counter + "_clicked", "0");
+    document.getElementById("cell" + counter).className = "";
+    document.getElementById("cell" + counter).innerHTML = actu_word;
+  }
 }
 
 function loadWordsFromStorage() {
-	for (let counter=0; counter<cells; counter++) {
-		var element = document.getElementById('cell' + counter);
-		var actu_word = localStorage.getItem("cell"+counter);
+  const randomizedWords = JSON.parse(localStorage.getItem("full_list"));
+  const language = localStorage.getItem("language");
 
-		element.innerHTML = actu_word; 
-		var already_clicked = "";
-		var x = localStorage.getItem("cell"+counter+"_clicked");
-		console.log(x)
-		if (x!=="0") {
-			element.className = "clicked";
-		}
-		
-	}
-	var first = document.getElementById('cell' + 0);
-	first.click();
-	first.click();
+  for (let counter = 0; counter < cells; counter++) {
+    var element = document.getElementById("cell" + counter);
+    //var actu_word = localStorage.getItem("cell" + counter);
+
+    element.innerHTML = randomizedWords[language][counter];
+    var already_clicked = "";
+    var x = localStorage.getItem("cell" + counter + "_clicked");
+    console.log(x);
+    if (x !== "0") {
+      element.className = "clicked";
+    }
+  }
+  var first = document.getElementById("cell" + 0);
+  first.click();
+  first.click();
 }
 
+function randomize() {
+  var randomizedWords = { de: [], en: [] };
+  var temp_index = 0;
 
+  for (const key of Object.keys(words.de)) {
+    var i = 0;
+    while (i < 2) {
+      temp_index = Math.floor(Math.random() * words.de[key].length);
+      if (!randomizedWords.de.includes(words.de[key][temp_index])) {
+        i++;
+        randomizedWords.de.push(words.de[key][temp_index]);
+        randomizedWords.en.push(words.en[key][temp_index]);
+      }
+    }
+  }
 
+  var length = randomizedWords.de.length;
+  while (length < 25) {
+    temp_index = Math.floor(Math.random() * allgemein.de.length);
+    if (!randomizedWords.de.includes(allgemein.de[temp_index])) {
+      length++;
+      randomizedWords.de.push(allgemein.de[temp_index]);
+      randomizedWords.en.push(allgemein.en[temp_index]);
+    }
+  }
 
+  return randomizedWords;
+}
 
-function randomize(words) {
-	var randomizedWords = [];
-	var tempWords = words.slice();
-	
-	for (var i=0; i<cells; i++) {
-		var wordCount = tempWords.length;
-		var randomIndex = Math.floor(Math.random() * wordCount);
-		var randomWord = tempWords[randomIndex];
-		randomizedWords.push(randomWord);
-		tempWords.splice(randomIndex,1);
-
-		if (tempWords.length==0) {
-			tempWords = words.slice();
-		}
-
-	}
-
-	return randomizedWords;
+function changeLang(event) {
+  var lang = event.value; // decide which language to display using switch case. The rest is obvious (i think)
+  console.log(lang)
+  switch (lang) {
+    case "en":
+      localStorage.setItem("language", "en");
+      $("[lang]").hide();
+      $('[lang="en"]').show();
+      break;
+    case "de":
+      localStorage.setItem("language", "de");
+      $("[lang]").hide();
+      $('[lang="de"]').show();
+      break;
+    default:
+      localStorage.setItem("language", "de");
+      $("[lang]").hide();
+      $('[lang="de"]').show();
+  }
+  loadWordsFromStorage();
 }
--- words.js
+++ words.js
@@ -1,41 +1,165 @@
-const words = `
+const allgemein = {
+  de: [
+  "Glühwein",
+  "Beschleuniger",
+  "Strahlung",
+  "Dosimeter",
+  "ATLAS",
+  "Tischkicker",
+  "ZIH-Cluster",
+  "Monte Carlo",
+  "Data Analysis",
+  "C++",
+  "Größenordnung",
+  "Quantenfeldtheorie",
+  "Loop",
+  "Next-to-Leading Order",
+  "Tree Level",
+  "Theorie",
+  "experimentell",
+  "Detektoren",
+  "Nuklidkarte",
+  "Astro-",
+  "Laser",
+  "Hadronen",
+  "Medizinphysik",
+  "Strahlenschutz",
+  "schwarzes Loch",
+  "Grundlagenforschung",
+],
 
-Vektor Boson Streuung
-Neutrinoloser Doppelbeta-Zerfall
-Glühwein
-Outreach
-Masterclass
-Beschleuniger
-Strahlung
-Dosimeter
-ATLAS
-Tischkicker
-ZIH-Cluster
-Monte Carlo
-Data Analysis
-C++
-Größenordnung
-Quantenfeldtheorie
-Loop
-Next-to-Leading Order
-Tree Level
-Theorie
-experimentell
-Detektoren
-CERN
-SNO
-Neutrinos
-Nuklidkarte
-Astro-
-Laser
-Hadronen
-Medizinphysik
-Strahlenschutz
-schwarzes Loch
-Grundlagenforschung
-Paper
-Angewandt
+en: [
+    "mulled wine",
+    "accelerator",
+    "radiation",
+    "dosimeter",
+    "ATLAS",
+    "table football",
+    "ZIH-Cluster",
+    "Monte Carlo",
+    "Data Analysis",
+    "C++",
+    "order of magnitude",
+    "quantum field theory",
+    "loop",
+    "next-to-leading order",
+    "tree level",
+    "theory",
+    "experiments",
+    "detectors",
+    "table of nuclides",
+    "astro-",
+    "laser",
+    "hadrons",
+    "medical physics",
+    "radiation protection",
+    "black hole",
+    "basic research",
+  ]}
 
+const words = {
+  de:{
+  Arno: ["Tau Leptonen", "Liquid Argon", "Kaffee & Kuchen"],
 
+  Frank: [
+    "Vektor Boson Streuung",
+    "SM ist unvollständig",
+    "Jemand gestikuliert Teilchenkollision",
+    "Polarisation",
+    "Machine Learning",
+    "UFO",
+    "Sherpa",
+    "Higgs-Mechanismus",
+  ],
 
-`;
+  Kai: [
+    "Doppelbeta Zerfall",
+    "Germanium Detektoren",
+    "Neutrino",
+    "Myon-Einfang",
+  ],
+
+  Uta: ["Jugendliche", "Lehrkräfte", "Outreach", "Masterclass"],
+
+  Felsenkeller: [
+    "Pelletron Beschleuniger",
+    "Sputter Ionquelle",
+    "Operatorschulung",
+    "Kuchen !",
+    "Gas-Jet-Target",
+    "The Big Bang Theory",
+  ],
+
+  Wallner: ["HAMSTER", "Isotop"],
+
+  Schramm: [
+    "Relativistische Transparenz",
+    "Surfer",
+    "Hochdichtes Plasma",
+    "Freie-Elektronen-Laser",
+    "Kritische Dichte",
+    "Injektion",
+    "Protonenstrahltherapie",
+    "Grafikkarte",
+  ],
+
+  Dominik: [
+    "Renormierung",
+    "Gamma-5",
+    "FlexibleSUSY",
+    "Lepton-Flavour-Verletzung",
+  ],
+},
+
+en:{
+    Arno: ["Tau Leptons", "Liquid Argon", "coffee"],
+  
+    Frank: [
+      "vector boson scattering",
+      "SM is incomplete",
+      "imitating a collision with hands",
+      "polarisation",
+      "Machine Learning",
+      "UFO",
+      "Sherpa",
+      "Higgs-Mechanism",
+    ],
+  
+    Kai: [
+      "Doppelbeta decay",
+      "Germanium detector",
+      "Neutrino",
+      "Muon-Capture",
+    ],
+  
+    Uta: ["Teens", "Teachers", "Outreach", "Masterclass"],
+  
+    Felsenkeller: [
+      "Pelletron accelerator",
+      "Sputter Ion Pump",
+      "Operatorschulung",
+      "Pie !",
+      "Gas-Jet-Target",
+      "The Big Bang Theory",
+    ],
+  
+    Wallner: ["HAMSTER", "Isotope"],
+  
+    Schramm: [
+      "relativistic transparency",
+      "Surfer",
+      "high-density Plasma",
+      "free electron Laser",
+      "critical density",
+      "Injection",
+      "proton beam therapy",
+      "graphics card",
+    ],
+  
+    Dominik: [
+      "Renormalization",
+      "Gamma-5",
+      "FlexibleSUSY",
+      "Lepton-Flavour-Violation",
+    ],
+  }}
